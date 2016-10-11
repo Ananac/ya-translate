f610ebe04b306a874f786fc3d77dd189755e2041
Ananac <6ananas9@gmail.com>
2016-10-11 21:50:05 +0300

    first version

---
+++ lib/translate/tranapi.rb
@@ -0,0 +1,29 @@
+module Translate
+  class TranApi
+    $baseurl = "https://translate.yandex.net/api/v1.5/tr.json/"
+
+    def initialize(key)
+      @key = key
+    end
+
+    def merge_url(path)
+     URI("#{$baseurl}#{path}")
+    end
+
+    def get_langs(ui)
+      path = "getLangs?ui=#{ui}&key=#{@key}"
+      return HTTParty.post(merge_url(path)).parsed_response["langs"]
+    end
+
+    def detect(text)
+      path = "detect?key=#{@key}"
+      return HTTParty.post(merge_url(path), body: {text: text}).parsed_response["lang"]
+    end
+
+    def trans(text, langfrom, langto)
+      lang = "#{langfrom}-#{langto}"
+      path = "/translate?key=#{@key}&lang=#{lang}"
+      return HTTParty.post(merge_url(path), body: {text: text}).parsed_response["text"]
+    end
+  end
+end
