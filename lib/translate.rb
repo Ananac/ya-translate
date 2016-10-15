79cfa200557a904471ecbcc93f5c89d7ff0b0554
Denis Bazylev <6ananas9@gmail.com>
2016-10-15 21:27:14 +0300

    1

--- lib/translate.rb
+++ lib/translate.rb
@@ -3,7 +3,6 @@ require "httparty"
 require "translate/tranapi"
 
 module Translate
-  translator = TranApi.new('trnsl.1.1.20160923T161439Z.a61c74b31a009a6a.77fea3fff49d34ac21d21d5155e39bbed4c21e9c')
-  puts translator.trans("can you take this pen?", "en", "ru")
+  
 end
 
