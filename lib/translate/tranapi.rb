b8bf6c083d3d72a4f89cd65a4073b7d4862d2a52
Denis Bazylev <6ananas9@gmail.com>
2016-11-13 17:02:22 +0300

    add error messages

--- lib/translate/tranapi.rb
+++ lib/translate/tranapi.rb
@@ -7,23 +7,33 @@ module Translate
     end
 
     def merge_url(path)
-     URI("#{$baseurl}#{path}")
+      URI("#{$baseurl}#{path}")
     end
 
     def get_langs(ui)
       path = "getLangs?ui=#{ui}&key=#{@key}"
-      return HTTParty.post(merge_url(path)).parsed_response["langs"]
+      response = HTTParty.post(merge_url(path)).parsed_response
+      check_errors(response)
+      response["langs"]
     end
 
     def detect(text)
       path = "detect?key=#{@key}"
-      return HTTParty.post(merge_url(path), body: {text: text}).parsed_response["lang"]
+      response = HTTParty.post(merge_url(path), body: {text: text}).parsed_response
+      check_errors(response)
+      response["lang"]
     end
 
     def trans(text, langfrom, langto)
       lang = "#{langfrom}-#{langto}"
       path = "/translate?key=#{@key}&lang=#{lang}"
-      return HTTParty.post(merge_url(path), body: {text: text}).parsed_response["text"]
+      response = HTTParty.post(merge_url(path), body: {text: text}).parsed_response
+      check_errors(response)
+      response["text"]
+    end
+
+    def check_errors(response)
+      abort(response["message"]) if response["code"] != 200 && response["code"] != nil
     end
   end
 end
