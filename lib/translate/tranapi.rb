deca57362d451af3e355e03dd4e07e2d7b6a7b2f
Denis Bazylev <6ananas9@gmail.com>
2016-11-17 16:46:08 +0300

    Alternate error msgs

--- lib/translate/tranapi.rb
+++ lib/translate/tranapi.rb
@@ -1,29 +1,32 @@
 module Translate
   class TranApi
-    $baseurl = "https://translate.yandex.net/api/v1.5/tr.json/"
+    $baseurl = 'https://translate.yandex.net/api/v1.5/tr.json/'
 
     def initialize(key)
       @key = key
     end
 
     def merge_url(path)
-     URI("#{$baseurl}#{path}")
+      URI("#{$baseurl}#{path}")
     end
 
     def get_langs(ui)
       path = "getLangs?ui=#{ui}&key=#{@key}"
-      return HTTParty.post(merge_url(path)).parsed_response["langs"]
+      response = HTTParty.post(merge_url(path)).parsed_response
+      return "#{response['langs']}#{response['message']}"
     end
 
     def detect(text)
       path = "detect?key=#{@key}"
-      return HTTParty.post(merge_url(path), body: {text: text}).parsed_response["lang"]
+      response = HTTParty.post(merge_url(path), body: {text: text}).parsed_response
+      return "#{response['lang']}#{response['message']}"
     end
 
     def trans(text, langfrom, langto)
       lang = "#{langfrom}-#{langto}"
       path = "/translate?key=#{@key}&lang=#{lang}"
-      return HTTParty.post(merge_url(path), body: {text: text}).parsed_response["text"]
+      response = HTTParty.post(merge_url(path), body: {text: text}).parsed_response
+      return "#{response['text']}#{response['message']}"
     end
   end
 end
